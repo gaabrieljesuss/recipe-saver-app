ac3b8e0ae3b9dd81a524990cf8369f2241976bac
gabrielsantos <gjs8@aluno.ifal.edu.br>
2022-12-02 09:44:50 -0300

    Add initial project

---
+++ recipe-saver-app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  recipe-saver-app
+//
+//  Created by Gabriel Santos on 02/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recipe-saver-app/recipe_saver_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_saver_appApp.swift
+//  recipe-saver-app
+//
+//  Created by Gabriel Santos on 02/12/22.
+//
+
+import SwiftUI
+
+@main
+struct recipe_saver_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
